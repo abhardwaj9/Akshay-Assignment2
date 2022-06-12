709b581b034003cf16d8709d4c8671d3cda21c66
AKSHAY BHARDWAJ <akshay01_2008@yahoo.com>
2022-06-11 20:44:48 -0400

    Create game.java
    A2

---
+++ src/com/coderscampus/game.java
@@ -0,0 +1,57 @@
+package com.coderscampus;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class game {
+
+	public static void main(String[] args) {
+		numbVerify();
+	}
+	
+	public static int getNumber() {
+		Scanner scanner = new Scanner(System.in);
+		boolean correctEntry = false;
+		int convertedInput = 0;
+		while(!correctEntry) {
+			System.out.print("Pick a number between 1 and 100 ");
+			String input = scanner.next();
+			convertedInput = Integer.parseInt(input);
+			if(convertedInput < 1 || convertedInput > 100) {
+				System.out.println("Your guess is not between 1 and 100, please try again");
+			}else {
+				correctEntry = true;
+			}
+		}
+		return convertedInput;
+		
+	}
+	public static void numbVerify() {
+				
+		int randomInt = (int)(100.0 * Math.random());	
+		
+		int guessedNumber = getNumber();
+		for(int i = 0; i < 4;i++) {
+			if(guessedNumber == randomInt) {
+				System.out.println("You win!!");
+				i=5;
+			}
+			else if(guessedNumber > randomInt) {
+				System.out.println("Please pick a lower number");
+				guessedNumber = getNumber();
+			}
+			else if(guessedNumber < randomInt) {
+				System.out.println("Please pick a Higher number");
+				guessedNumber = getNumber();
+			}
+			if(i==3){
+				System.out.println("You loose!!");
+				System.out.println("The number to guess was: " + randomInt);
+			}
+		}
+	
+	}
+}
+
+
