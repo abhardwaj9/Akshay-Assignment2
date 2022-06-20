64c903702e9b33f75b80fe31bcf0d1baaab3f570
AKSHAY BHARDWAJ <akshay01_2008@yahoo.com>
2022-06-20 11:55:55 -0400

    Update Game.java
    a2

--- src/com/coderscampus/Game.java
+++ src/com/coderscampus/Game.java
@@ -1,57 +1,53 @@
 package com.coderscampus;
 
-import java.util.Random;
 import java.util.Scanner;
 
-
-public class game {
+public class Game {
 
 	public static void main(String[] args) {
-		numbVerify();
+		Game game = new Game();
+		game.numbVerify();
 	}
-	
-	public static int getNumber() {
+
+	private int getNumber() {
 		Scanner scanner = new Scanner(System.in);
 		boolean correctEntry = false;
 		int convertedInput = 0;
-		while(!correctEntry) {
+		while (!correctEntry) {
 			System.out.print("Pick a number between 1 and 100 ");
 			String input = scanner.next();
 			convertedInput = Integer.parseInt(input);
-			if(convertedInput < 1 || convertedInput > 100) {
+			if (convertedInput < 1 || convertedInput > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
-			}else {
+			} else {
 				correctEntry = true;
 			}
 		}
 		return convertedInput;
-		
+
 	}
-	public static void numbVerify() {
-				
-		int randomInt = (int)(100.0 * Math.random());	
-		
+
+	private void numbVerify() {
+
+		int randomInt = (int) (100.0 * Math.random());
+
 		int guessedNumber = getNumber();
-		for(int i = 0; i < 4;i++) {
-			if(guessedNumber == randomInt) {
+		for (int i = 0; i < 4; i++) {
+			if (guessedNumber == randomInt) {
 				System.out.println("You win!!");
-				i=5;
-			}
-			else if(guessedNumber > randomInt) {
+				i = 5;
+			} else if (guessedNumber > randomInt) {
 				System.out.println("Please pick a lower number");
 				guessedNumber = getNumber();
-			}
-			else if(guessedNumber < randomInt) {
+			} else if (guessedNumber < randomInt) {
 				System.out.println("Please pick a Higher number");
 				guessedNumber = getNumber();
 			}
-			if(i==3){
+			if (i == 3) {
 				System.out.println("You loose!!");
 				System.out.println("The number to guess was: " + randomInt);
 			}
 		}
-	
+
 	}
 }
-
-
